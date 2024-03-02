5712930ada29df0bece0cc3cee3b6beb9902b29d
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-02 18:43:09 +0200

    Added index View and template

--- jobsPy/main/views.py
+++ jobsPy/main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "core/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # jobseekers = JobSeeker.objects.all().count()
+        # company = CompanyProfile.objects.all().count()
+        # jobs = Job.objects.all().count()
+        # last_jobs = Job.objects.all().order_by('pk')[:4]
+
+
+        # context["jobseekers"] = jobseekers
+        # context["company"] = company
+        # context["coutries"] = jobs
+        # context["last_jobs"] = last_jobs
+        # context["title"] = "JobsPy - Your Future Begins Here"
+        return context

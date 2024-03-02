4408018da30a6f56fe73f36d7a362be55682db68
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-02 17:56:49 +0200

    Added accounts forms and views. Login, Register and Change Password

---
+++ jobsPy/accounts/forms.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+
+UserModel = get_user_model()
+
+
+class RegisterUserForm(UserCreationForm):
+    # ROLE_CHOICES = (
+    #     ('jobseeker', 'Job Seekeker'),
+    #     ('company', 'Company'),
+    # )
+    #
+    # role = ChoiceField(choices=ROLE_CHOICES)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+        self.fields['email'].widget.attrs.update({
+                 'placeholder': 'Enter your email'})
+
+        self.fields['password1'].widget.attrs.update({
+                 'placeholder': 'Enter your password'})
+
+        self.fields['password2'].widget.attrs.update({
+                 'placeholder': 'Enter your password'})
+
+        self.fields['role'].label = "Profile type"
+
+    # def save(self, commit=True):
+    #     # user = super().save(commit=False)
+    #     user = super().save(commit=commit)
+    #     role = self.cleaned_data['role']
+    #
+    #     if role == 'jobseeker':
+    #         profile = JobSeeker(user=user)
+    #     elif role == 'company':
+    #         profile = CompanyProfile(user=user)
+    #     else:
+    #         raise ValueError('Invalid role selected')
+    #
+    #     if commit:
+    #         profile.save()
+    #
+    #     return user
+
+    class Meta:
+        model = UserModel
+        fields = ["email", "password1", "password2", "role"]
+
+
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+        self.fields['username'].widget.attrs.update({
+                 'placeholder': 'Enter your email'})
+
+        self.fields['password'].widget.attrs.update({
+                 'placeholder': 'Enter your password'})
+
+
+class ChangePassword(PasswordChangeForm):
+    class Meta:
+        model = UserModel
--- jobsPy/accounts/views.py
+++ jobsPy/accounts/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth import get_user_model, login, alogout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 
-# Create your views here.
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from jobsPy.accounts.forms import RegisterUserForm, LoginForm, ChangePassword
+
+userMode = get_user_model()
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = RegisterUserForm
+
+    # Static way of providing `success_url`
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+
+        login(self.request, self.object)
+
+        return result
+
+
+@login_required(login_url='login')
+def singout(request):
+    auth.logout(request)
+     # messages.success(request, 'You are logged out.')
+    return redirect('index')
+
+# def singout(request):
+#     alogout(request)
+    # Redirect to a success page.
+# class singout(LogoutView):
+#     template_name = "logout.html"
+#
+#
+
+class LoginUserView(LoginView):
+    template_name = "login.html"
+    form_class = LoginForm
+
+    # def get_success_url(self):
+    #     if self.request.user.role == "jobseeker":
+    #         return reverse_lazy("index")
+    #     return super().get_success_url()
+
+
+
+class ChangePass(PasswordChangeView):
+    form_class = ChangePassword
+
+    template_name = "chage_password.html"
+    success_url = reverse_lazy('login_redirect_dashboard')

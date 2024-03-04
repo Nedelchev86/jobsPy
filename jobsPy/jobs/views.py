5d91c4150564628688e84917669603b863827653
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-04 20:06:45 +0200

    Added JobCreateView

--- jobsPy/jobs/views.py
+++ jobsPy/jobs/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from jobsPy.core.accounts_mixins import CompanyRoleRequiredMixin
+
+
+class JobCreateView(LoginRequiredMixin, CompanyRoleRequiredMixin, CreateView):
+    template_name = "create_job.html"
+    form_class = CreateJobForms
+    extra_context = {"title": "Post New Job"}
+    success_url = reverse_lazy("index")

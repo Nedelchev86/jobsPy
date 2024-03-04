83bf15cf1e7e1156d39c536187768e84e91985b2
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-04 19:50:35 +0200

    Added Company model

--- jobsPy/company/models.py
+++ jobsPy/company/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+UserModel = get_user_model()
+
+class CompanyProfile(models.Model):
+    user = models.OneToOneField(UserModel, on_delete=models.CASCADE, related_name="company")
+    name = models.CharField(max_length=40)
+    description = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=40)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    image = models.ImageField(
+        null=True, blank=True, upload_to="company/image")
+    website_url = models.URLField(max_length=200)
+
+    @property
+    def get_all_applicant(self):
+
+        retur

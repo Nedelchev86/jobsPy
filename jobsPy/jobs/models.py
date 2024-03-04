f84f3727c5b2553400fa38fc59ec3f6b15fd3cae
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-04 20:02:21 +0200

    Added Jobs Category model

--- jobsPy/jobs/models.py
+++ jobsPy/jobs/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.crypto import get_random_string
 
 # Create your models here.
+
+UserModel = get_user_model()
+
+
+class Skills(models.Model):
+    name = models.CharField(blank=True, null=True, max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+def unique_slugify(instance, slug):
+    model = instance.__class__
+    unique_slug = slug
+    while model.objects.filter(slug=unique_slug).exists():
+        unique_slug = slug + get_random_string(length=5)
+    return unique_slug
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(allow_unicode=True, max_length=100, unique=True)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.slug:
+            self.slug = slugify(self.name, allow_unicode=True)
+        return super().save(*args, **kwargs)

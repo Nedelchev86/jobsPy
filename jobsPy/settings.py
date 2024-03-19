9defc47127e9baa25ced6d842072b58b0062de9c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-19 21:15:46 +0200

    Set up environment variables

--- jobsPy/settings.py
+++ jobsPy/settings.py
@@ -10,15 +10,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#@+a*dqxbh^fs7y)+_6$j^4$5!%)b25)p!r57@ffnz=u%45ooj'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
+# SECRET_KEY = 'django-insecure-#@+a*dqxbh^fs7y)+_6$j^4$5!%)b25)p!r57@ffnz=u%45ooj'
+SECRET_KEY = os.getenv('SECRET_KEY', None)
+
 # DEBUG = False
 #
 # ALLOWED_HOSTS = ["localhost",]
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 ALLOWED_HOSTS = []
 
 # Application definition

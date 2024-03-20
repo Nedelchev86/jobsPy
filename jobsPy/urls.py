adc2cf3357192ff8e27d3667a814f9007de76d13
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-20 17:59:29 +0200

    Set up environment variables

--- jobsPy/urls.py
+++ jobsPy/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('company/', include('jobsPy.company.urls')),
     path('jobs/', include('jobsPy.jobs.urls')),
     path('contact/', Contact.as_view(), name="contact"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,

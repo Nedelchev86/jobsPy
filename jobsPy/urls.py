70faa5a2d38f1ac3fa5b3fb76d8267c604b2e255
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-24 19:11:47 +0200

    Handle 403

--- jobsPy/urls.py
+++ jobsPy/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from jobsPy.main.views import Contact
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler403
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,3 +21,5 @@ urlpatterns = [
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
+
+handler403 = 'jobsPy.main.views.custom_403'

f808e62f262b9ee37e9c92bd71fc2b1c4086fd0c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-25 22:33:08 +0200

    Added DRF

---
+++ static/js/blog_detail.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Extract blog ID from URL
+
+    const urlParts = window.location.pathname.split('/');
+    const blogId = urlParts[urlParts.length - 2];  // Assuming the ID is the second-to-last part of the URL
+
+    // Make API call to fetch blog post details
+    fetch(`/api/blog/${blogId}/`)
+        .then(response => response.json())
+        .then(blog => {
+            const mainImage = document.getElementById('main-image')
+            mainImage.src = blog.image_url_1
+            const secondImage = document.getElementById('second-image')
+            secondImage.src = blog.image_url_2
+            // Render blog post details on the webpage
+//            const blogDetails = document.getElementById('blog-details');
+//            blogDetails.innerHTML = `
+//                <h1>${blog.title}</h1>
+//                <p>${blog.description}</p>
+//                <!-- Other blog post details -->
+//            `;
+        })
+        .catch(error => {
+            console.error('Error fetching blog post:', error);
+        });
+});

160e8b7088b18ebf7d988649bd68ae1b2bfc9d74
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-26 19:12:11 +0200

    Added DRF

--- jobsPy/blog/admin.py
+++ jobsPy/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from jobsPy.blog.models import BlogPost
+
+
 # Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    pass

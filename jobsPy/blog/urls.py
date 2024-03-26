26d65e08cad38a4c0e0cd052aba9f31c4ca59fc2
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-26 23:11:08 +0200

    Added permission and POST method

--- jobsPy/blog/urls.py
+++ jobsPy/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from jobsPy.blog.views import BlogList, SingleBlog
+from jobsPy.blog.views import BlogList, SingleBlog, CreateBlog
 
 urlpatterns = [
     # path('blog/', BlogPostListCreateAPIView.as_view(), name='blog-list'),
     # path('blog/<int:pk>/', BlogPostRetrieveUpdateDestroyAPIView.as_view(), name='blog-detail'),
     path('', BlogList.as_view(), name='blog-list'),
     path('<int:pk>/', SingleBlog.as_view(), name='blog-detail'),
+    path('add/', CreateBlog.as_view(), name='blog-create'),
     # Other URL patterns...
 ]

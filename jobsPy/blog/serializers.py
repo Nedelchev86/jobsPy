cae50c917e4be7d019713870b88412a66f957513
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-28 21:28:40 +0200

    Added comments system

--- jobsPy/blog/serializers.py
+++ jobsPy/blog/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from jobsPy.blog.models import BlogPost
+from jobsPy.blog.models import BlogPost, Comment
 from jobsPy.jobseekers.models import JobSeeker
 
 
@@ -11,8 +11,18 @@ class JobSeekerSerializer(serializers.ModelSerializer):
         fields = ['id', 'first_name', 'last_name']  # Add other fields as needed
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(read_only=True)  # Assuming you only want to show the author's ID
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at']
+
 class BlogPostSerializer(serializers.ModelSerializer):
     author = JobSeekerSerializer()  # Nested serializer for JobSeeker
+    comments = CommentSerializer(many=True, read_only=True)  # Nested serializer for comments
+
     class Meta:
         model = BlogPost
-        fields = ['id', 'title', 'description', 'image_url_1', 'image_url_2', 'image_url_3', 'views', 'created_at', 'author']
+        fields = ['id', 'title', 'description', 'image_url_1', 'image_url_2', 'image_url_3', 'views', 'created_at', 'author', 'comments']
+

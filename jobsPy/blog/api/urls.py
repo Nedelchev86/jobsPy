2d014a8f4ec9948ad9f43a24213e58314aaeaeed
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-28 21:28:43 +0200

    Added comments system

--- jobsPy/blog/api/urls.py
+++ jobsPy/blog/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from jobsPy.blog.views import BlogPostListCreateAPIView, BlogPostRetrieveUpdateDestroyAPIView
+from jobsPy.blog.views import BlogPostListCreateAPIView, BlogPostRetrieveUpdateDestroyAPIView, CommentListCreateAPIView, \
+    CommentRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('blog/', BlogPostListCreateAPIView.as_view(), name='blog-list'),
     path('blog/<int:pk>/', BlogPostRetrieveUpdateDestroyAPIView.as_view(), name='blog-detail'),
-    # Other URL patterns...
+    path('blog/<int:pk>/comments/', CommentListCreateAPIView.as_view(), name='comment-list'),
+    path('blog/<int:pk>/comments/<int:comment_pk>/', CommentRetrieveUpdateDestroyAPIView.as_view(), name='comment-detail'),
 ]
+

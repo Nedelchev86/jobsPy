f60516651e5b076ade21bf47fbfc1cfe88338abe
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-28 20:40:52 +0200

    Added single post more information

--- jobsPy/blog/serializers.py
+++ jobsPy/blog/serializers.py
@@ -1,9 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from jobsPy.blog.models import BlogPost
+from jobsPy.jobseekers.models import JobSeeker
+
+
+class JobSeekerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobSeeker
+        fields = ['id', 'first_name', 'last_name']  # Add other fields as needed
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
+    author = JobSeekerSerializer()  # Nested serializer for JobSeeker
     class Meta:
         model = BlogPost
-        fields = ['id', 'title', 'description', 'image_url_1', 'image_url_2', 'views', 'created_at']
+        fields = ['id', 'title', 'description', 'image_url_1', 'image_url_2', 'image_url_3', 'views', 'created_at', 'author']

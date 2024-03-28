0554ddddb10ab9337e6e0b1b32ef2903b3a2fe72
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-28 21:28:29 +0200

    Added comments system

--- jobsPy/blog/views.py
+++ jobsPy/blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from jobsPy.blog.models import BlogPost
+from jobsPy.blog.models import BlogPost, Comment
 from jobsPy.blog.permission import IsAuthor
-from jobsPy.blog.serializers import BlogPostSerializer
+from jobsPy.blog.serializers import BlogPostSerializer, CommentSerializer
 
 
 # Create your views here.
@@ -42,7 +43,32 @@ class SingleBlog(TemplateView):
     template_name = 'blog/single-blogs.html'
 
 
-
 class CreateBlog(TemplateView):
     template_name = 'blog/create-blog.html'
     permission_classes = [IsAuthor()]
+
+
+class CommentListCreateAPIView(generics.CreateAPIView):
+
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Retrieve the post ID from the URL parameters
+        post_id = self.kwargs.get('pk')
+        # Filter comments based on the specified post ID
+        print(post_id)
+        return Comment.objects.filter(pk=post_id)
+
+    def perform_create(self, serializer):
+        # Retrieve the post ID from the URL parameters
+        post_id = self.kwargs.get('pk')
+        # Retrieve the blog post object based on the post ID
+        blog_post = BlogPost.objects.get(pk=post_id)
+        # Set the post field of the comment to the blog post object
+        serializer.save(author=self.request.user, post=blog_post)
+        serializer.save(author=self.request.user)
+
+class CommentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer

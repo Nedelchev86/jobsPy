bc9eaecdda832a452bdf31709d04f29c85543c9b
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-29 22:52:52 +0200

    Added signal to activate jobseeker when edit profile

--- jobsPy/jobseekers/apps.py
+++ jobsPy/jobseekers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class JobseekersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jobsPy.jobseekers'
+
+    def ready(self):
+        import jobsPy.jobseekers.signals

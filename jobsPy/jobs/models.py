1106fc044eecd76a31cb0e7511511fbfd00d24f8
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-29 20:37:38 +0200

    Added CKEditor in create job

--- jobsPy/jobs/models.py
+++ jobsPy/jobs/models.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.crypto import get_random_string
@@ -48,13 +49,14 @@ JOB_TYPE = (
     ('internship', "Internship"),
 )
 
+
 class Job(models.Model):
     user = models.ForeignKey(UserModel,  on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
     slug = models.SlugField(allow_unicode=True, max_length=100, unique=True)
     category = models.ForeignKey(Category,  on_delete=models.CASCADE)
     seniority = models.ForeignKey(Seniority, on_delete=models.SET_NULL, blank=True, null=True)
-    description = models.TextField()
+    description = RichTextField(null=False)
     job_image = models.ImageField(null=True, blank=True, upload_to="jobs/image", default="images/default/default.jpg")
     vacancy = models.IntegerField(null=True, blank=True)
     location = models.CharField(max_length=300,)

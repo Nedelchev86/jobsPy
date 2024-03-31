c7f4e795efd253e004caceeb28a14de17babbce8
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-31 07:17:58 +0300

    Added blog list pagination

--- jobsPy/settings.py
+++ jobsPy/settings.py
@@ -147,7 +147,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # if not DEBUG:
 #     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)

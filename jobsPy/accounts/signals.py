51c38d30fbd63c7d2c77f62b76603547e59a4fc2
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-31 14:18:53 +0300

    Added Google and GitHub login

--- jobsPy/accounts/signals.py
+++ jobsPy/accounts/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from jobsPy.company.models import CompanyProfile
 from jobsPy.jobseekers.models import JobSeeker
+from allauth.account.signals import user_logged_in
 
 userModel = get_user_model()
 
@@ -17,8 +18,26 @@ def create_user_profile(sender, instance, created, **kwargs):
         if instance.role == 'jobseeker':
             JobSeeker.objects.create(user=instance)
 
-        elif instance.role == 'company':
+        else:
             CompanyProfile.objects.create(user=instance)
 
 
 
+
+
+# myapp/signals.py
+
+from django.dispatch import receiver
+from allauth.account.signals import user_logged_in
+from django.shortcuts import redirect
+
+@receiver(user_logged_in)
+def handle_user_logged_in(sender, request, user, **kwargs):
+    # Check if the user's role is already set
+
+    if not user.role:
+        # user.role = 'jobseeker'
+        # user.save()
+
+        # Redirect the user to select their role
+        return redirect('select_role')  # Adjust the URL name as per your project's URL configuration

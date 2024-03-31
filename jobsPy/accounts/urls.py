5c5294d4910de53ea31ed38714539568578d1f11
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-03-31 14:18:53 +0300

    Added Google and GitHub login

--- jobsPy/accounts/urls.py
+++ jobsPy/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from jobsPy.accounts.views import RegisterView, ChangePass, LoginUserView, singout
+from jobsPy.accounts.views import RegisterView, ChangePass, LoginUserView, singout, select_role
 
 urlpatterns = [
     path("registration/", RegisterView.as_view(), name="registration"),
     path('login/', LoginUserView.as_view(), name="login"),
     path("logout/", singout, name="sing-out"),
     path("chage-password/", ChangePass.as_view(), name="change-password"),
+    path('select-role/', select_role, name='select_role'),
 ]

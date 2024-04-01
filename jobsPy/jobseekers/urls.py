6a097fd510d2cf4c5079639c17a2bce8b4338b44
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-01 23:10:32 +0300

    Little design and vizualization changes

--- jobsPy/jobseekers/urls.py
+++ jobsPy/jobseekers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from jobsPy.jobseekers.views import JobSeekerDashboard, EditProfile, FavouriteJobs, ApplyJobs, JobSeekerDetails, \
-    AllEmployees, AddEducation, AddWorkExperience, EditEducation
+    AllEmployees, AddEducation, AddWorkExperience, EditEducation, EditWorkExperience
 
 urlpatterns = [
     path("all/", AllEmployees.as_view(), name="all-employees"),
@@ -11,6 +11,7 @@ urlpatterns = [
     path("add-education/<int:pk>/", AddEducation.as_view(), name="add-education"),
     path("edit-education/<int:pk>/", EditEducation.as_view(), name="edit-education"),
     path("add-work-experience/<int:pk>/", AddWorkExperience.as_view(), name="add-work-experience"),
+    path("edit-work-experience/<int:pk>/", EditWorkExperience.as_view(), name="edit-work-experience"),
     # path("edit-profile/", EditProfile.as_view(), name="edit-profile"),
     path("favourite-jobs/", FavouriteJobs.as_view(), name="favourite_jobs"),
     path("apply-jobs/", ApplyJobs.as_view(), name="jobs-apply"),

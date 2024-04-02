7c4bd278d0359987933d594dcefa2cea3ca7236f
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-02 22:45:55 +0300

    Added address, linkedin url, activated boolean field and more for Company Model

--- jobsPy/company/models.py
+++ jobsPy/company/models.py
@@ -8,13 +8,18 @@ UserModel = get_user_model()
 
 class CompanyProfile(models.Model):
     user = models.OneToOneField(UserModel, on_delete=models.CASCADE, primary_key=True, related_name="company")
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, null=False, blank=False)
     description = RichTextField(null=False, blank=False)
-    location = models.CharField(max_length=40)
+    location = models.CharField(max_length=40, null=False, blank=False)
+    address = models.CharField(null=False, blank=False, max_length=200)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    image = models.ImageField(
-        null=True, blank=True, upload_to="company/image")
-    website_url = models.URLField(max_length=200)
+    image = models.ImageField(null=True, blank=True, upload_to="company/image")
+    website_url = models.URLField(max_length=200, null=True, blank=True)
+    linkedin_url = models.URLField(max_length=200, null=True, blank=True)
+    facebook_url = models.URLField(max_length=200, null=True, blank=True)
+    employees = models.PositiveIntegerField(null=False, blank=False)
+    year_of_foundation = models.PositiveIntegerField(null=False, blank=False)
+    activated = models.BooleanField(default=False)
 
 
     @property

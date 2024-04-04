3aead728d5b3bc9462f17e968547c4b2023a2182
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-04 16:34:37 +0300

    JS optimization

--- static/js/blog/load_comment.js
+++ static/js/blog/load_comment.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
- function loadComment(blogPostId) {
+function loadComment(blogPostId) {
     fetch(`/api/blog/${blogPostId}/comments/`)
         .then(response => response.json())
         .then(data => {
-            const commentNumber = document.getElementById("comment-number")
-            commentNumber.textContent = `${data.length}  comments `
+            const commentNumber = document.getElementById("comment-number");
+            commentNumber.textContent = `${data.length}  comments`;
 
             // Update HTML with comments
             var commentsList = document.getElementById('comments-list');
@@ -30,21 +30,44 @@ document.addEventListener('DOMContentLoaded', function() {
                     image = comment.author.image;
                 }
 
-                commentElement.innerHTML = `
-                    <div class="comment-img">
-                        <img src=${image} class="rounded-circle" alt="img">
-                    </div>
-                    <div class="comment-desc">
-                        <div class="desc-top">
-                            <h6>${name}</h6>
-                            <span class="date">${comment.created_at}</span>
-                            <a href="#" class="reply-link"><i class="lni lni-reply"></i>Reply ( ToDo )</a>
-                        </div>
-                        <p>
-                            ${comment.content}
-                        </p>
-                    </div>
-                `;
+                // Create comment elements
+                var commentImg = document.createElement('div');
+                commentImg.classList.add('comment-img');
+                var img = document.createElement('img');
+                img.src = image;
+                img.classList.add('rounded-circle');
+                img.alt = 'img';
+                commentImg.appendChild(img);
+
+                var commentDesc = document.createElement('div');
+                commentDesc.classList.add('comment-desc');
+
+                var descTop = document.createElement('div');
+                descTop.classList.add('desc-top');
+                var h6 = document.createElement('h6');
+                h6.textContent = name;
+                var span = document.createElement('span');
+                span.classList.add('date');
+                span.textContent = comment.created_at;
+                var a = document.createElement('a');
+                a.href = '#';
+                a.classList.add('reply-link');
+                var i = document.createElement('i');
+                i.classList.add('lni', 'lni-reply');
+                a.appendChild(i);
+                a.textContent = 'Reply ( ToDo )';
+                descTop.appendChild(h6);
+                descTop.appendChild(span);
+                descTop.appendChild(a);
+
+                var p = document.createElement('p');
+                p.textContent = comment.content;
+
+                commentDesc.appendChild(descTop);
+                commentDesc.appendChild(p);
+
+                commentElement.appendChild(commentImg);
+                commentElement.appendChild(commentDesc);
 
                 commentsList.appendChild(commentElement);
             });
@@ -53,54 +76,3 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error fetching comments:', error);
         });
 }
-
-
-    // Fetch comments for the current blog post
-//   export function loadComment(blogPostId) {
-//    fetch(`/api/blog/${blogPostId}/comments/`)
-//        .then(response => response.json())
-//        .then(data => {
-//
-//            // Update HTML with comments
-//            var commentsList = document.getElementById('comments-list');
-//            data.forEach(comment => {
-//                var commentElement = document.createElement('li');
-//
-//                let name =  "Anonymous"
-//                if (comment.author.first_name){
-//                    name = comment.author.first_name}
-//                else if(comment.author.title){
-//                     name = comment.author.title
-//                          }
-//                let image = "/static/images/clients/default_profile.png"
-//                if (comment.author.profile_picture){
-//                    image = comment.author.profile_picture}
-//                 else if (comment.author.image){
-//                    image = comment.author.image}
-//
-//
-//
-//                commentElement.innerHTML  = `
-//                                                        <div class="comment-img">
-//                                            <img src=${image} class="rounded-circle" alt="img">
-//                                        </div>
-//                                        <div class="comment-desc">
-//                                            <div class="desc-top">
-//                                                <h6>${name}</h6>
-//
-//                                                <span class="date">${comment.created_at}</span>
-//                                                <a href="#" class="reply-link"><i class="lni lni-reply"></i>Reply</a>
-//                                            </div>
-//                                            <p>
-//                                               ${comment.content}
-//                                            </p>
-//                                        </div>
-//                `
-//
-//                commentsList.appendChild(commentElement);
-//            });
-//        })
-//        .catch(error => {
-//            console.error('Error fetching comments:', error);
-//        });}
-//});

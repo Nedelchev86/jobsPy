939fe09c19217b9857def26bfa2d856b891bb3a0
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-06 22:29:57 +0300

    Added custom templates for reset password

--- jobsPy/main/tasks.py
+++ jobsPy/main/tasks.py
@@ -3,8 +3,3 @@ from django.core.mail import send_mail
 
 from jobsPy import settings
 
-
-# @shared_task
-# def send_async_email(subject, message, recipient_list):
-#     sender_email = settings.EMAIL_HOST_USER
-#     send_mail(subject, message, sender_email, recipient_list)

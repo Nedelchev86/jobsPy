bd020a2a3ca14368ee7c42729843105eb17eddc2
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-06 17:38:21 +0300

    Added task to send mail

--- jobsPy/accounts/tests.py
+++ jobsPy/accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+from celery import shared_task
+from django.core.mail import send_mail
+
+@shared_task
+def send_async_email(subject, message, recipient_list):
+    send_mail(subject, message, 'your_email@example.com', recipient_list)

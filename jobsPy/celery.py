479e41bb881245374af9541d9ecad0ced380472d
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-06 17:40:48 +0300

    Added celery

---
+++ jobsPy/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobsPy.settings')
+
+
+app = Celery("jobsPy")
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+

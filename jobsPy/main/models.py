80cfc58b76628aba2c41b0309cefbdf9d2bb0f04
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-07 08:31:15 +0300

    Added contact form to send mail to site team and user wtih celery

--- jobsPy/main/models.py
+++ jobsPy/main/models.py
@@ -6,3 +6,16 @@ class Seniority(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=200, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    processed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subject

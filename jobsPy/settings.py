1bb271b5315ab66822bc7fc549313f14e3eb2528
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-07 20:22:42 +0300

    Added CSRF_TRUSTED_ORIGINS

--- jobsPy/settings.py
+++ jobsPy/settings.py
@@ -15,12 +15,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-
-
-# SECRET_KEY = 'django-insecure-#@+a*dqxbh^fs7y)+_6$j^4$5!%)b25)p!r57@ffnz=u%45ooj'
 SECRET_KEY = os.getenv('SECRET_KEY', None)
 
 # DEBUG = os.getenv('DEBUG', False)
@@ -70,22 +64,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'jobsPy.urls'
 
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [BASE_DIR / 'templates']
-#         ,
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
 
 TEMPLATES = [
     {
@@ -165,23 +143,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
-#
-# STATIC_ROOT = BASE_DIR / "static"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 AUTH_USER_MODEL = 'accounts.Account'
-
 
 LOGIN_URL = reverse_lazy("login")
 
@@ -194,43 +161,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # WHITENOISE_SKIP_COMPRESS_EXTENSIONS = []
 
-# if not DEBUG:
-#     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
-#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     # and renames the files with unique names for each version to support long-term caching
-#     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# else:
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
-
-# if not DEBUG:
-#     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     and renames the files with unique names for each version to support long-term caching
-#
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-#
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
-
-# if DEBUG:
-#
-#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#
-# else:
-#
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
-
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -240,8 +176,8 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
-# CSRF_TRUSTED_ORIGINS = ['https://localhost', 'http://localhost', 'http://http://127.0.0.1:8000/', 'http://127.0.0.1:8000/', 'https://jobspy.azurewebsites.net/']
-# CSRF_TRUSTED_ORIGINS = ['https://jobspy.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(' ')
+
 
 # AUTHENTICATION_BACKENDS = [
 #     'django.contrib.auth.backends.ModelBackend',

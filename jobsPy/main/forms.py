8988217bd877e5ad64386fa58a604f398dc505e7
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-07 10:33:04 +0300

    Added newsletter form with context_processors.py in all templates

---
+++ jobsPy/main/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm, EmailInput
+
+from jobsPy.main.models import Subscriber
+
+
+class SubscriberForm(ModelForm):
+    class Meta:
+        model = Subscriber
+        fields = ['email']
+
+        widgets = {
+            'email': EmailInput(attrs={'class': 'common-input',
+                                             'placeholder': 'Your email address',
+                                             'onfocus': "this.placeholder = ''",
+                                             'onblur': "this.placeholder = 'Your email address'",
+                                             'required': True,
+                                             'autocomplete': 'email'}),
+        }
--- jobsPy/main/models.py
+++ jobsPy/main/models.py
@@ -12,10 +12,19 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     subject = models.CharField(max_length=200, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
-    phone = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100, null=True, blank=True)
     message = models.TextField(null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     processed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.subject
+
+
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email

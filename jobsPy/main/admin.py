5706743d9544dc4e2f50084d4710acfddcae0f7d
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-07 10:32:01 +0300

    Register subscriber to admin

--- jobsPy/main/admin.py
+++ jobsPy/main/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from jobsPy.main.models import Seniority
+from jobsPy.main.models import Seniority, Contact, Subscriber
 
 
 # Register your models here.
 @admin.register(Seniority)
 class SeniorityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
     pass

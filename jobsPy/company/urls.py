1dfb03bf3a2be8c0b24c2e68a1f21ccec04b0d6d
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-10 09:30:05 +0300

    Add delete and edint functionality

--- jobsPy/company/urls.py
+++ jobsPy/company/urls.py
@@ -6,7 +6,7 @@ from jobsPy.jobs.views import ChangeStatus
 urlpatterns = [
     path('dashboard/', CompanyDashboard.as_view(), name="company-dashboard"),
     path('created-jobs/', CreatedJobs.as_view(), name="created-jobs"),
-    path('edit-company/<int:pk>/', EditCompany.as_view(), name="edit-company-profile"),
+    path('edit-company/', EditCompany.as_view(), name="edit-company-profile"),
     path('company-applicant/', CompanyApplicant.as_view(), name="jobs-applicants"),
     path('company-applicant/jobs-aplicant/<int:pk>/', ApplicantList.as_view(), name="applicant_list"),
     path('change-status/<int:pk>/', ChangeStatus.as_view(), name="change-status"),

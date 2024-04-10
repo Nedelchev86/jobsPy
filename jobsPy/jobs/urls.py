78c9f186f85f8a4d59032cac7d2295a670af1918
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-10 09:30:09 +0300

    Add delete and edint functionality

--- jobsPy/jobs/urls.py
+++ jobsPy/jobs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from jobsPy.jobs.views import JobCreateView, JobDetails, EditJob, AllJobsView, \
-    RemoveFromFavoritesView, add_to_favorites, apply_for_job
+    RemoveFromFavoritesView, add_to_favorites, apply_for_job, DeleteJob
 from jobsPy.jobseekers.views import FavouriteJobs, ApplyJobs
 from jobsPy.main.views import JobsCategory
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", AllJobsView.as_view(), name="all-jobs"),
     path('create-job/', JobCreateView.as_view(), name="create_job"),
     path('edit-job/<int:pk>/', EditJob.as_view(), name="edit_job"),
+    path('delete-job/<int:pk>/', DeleteJob.as_view(), name="delete job"),
     path("job-details/<int:pk>/", JobDetails.as_view(), name="job_details"),
     path("add-to-favourite/<int:pk>/", add_to_favorites, name="add-to-favourite"),
     path("remove-from-favourite/<int:pk>/", RemoveFromFavoritesView.as_view(), name="remove_From_fav"),

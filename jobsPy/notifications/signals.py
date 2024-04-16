3e4e2f95bc15253b630341c61842db456e9ab831
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-16 21:53:19 +0300

    Added notification for the job seeker when the company changes the status of their application.

--- jobsPy/notifications/signals.py
+++ jobsPy/notifications/signals.py
@@ -1,12 +1,20 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from jobsPy.jobs.models import Applicant
-from jobsPy.notifications.models import Notification
+from jobsPy.notifications.models import Notification, NotificationJobSeeker
 
 
 @receiver(post_save, sender=Applicant)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         job_owner = instance.job.user
-        message = f"{instance.user.jobseeker} has applied for the job: {instance.job.title}"
-        Notification.objects.create(user=job_owner, job=instance.job, message=message)
+        message = f" has applied for the job: "
+        Notification.objects.create(user=job_owner, job=instance.job, job_seeker=instance.user,  message=message)
+    else:
+        NotificationJobSeeker.objects.create(
+            user=instance.user,
+            job=instance.job,
+            status=instance.get_status(),
+            comment=instance.comment,
+
+        )
--- jobsPy/notifications/views.py
+++ jobsPy/notifications/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
-from jobsPy.notifications.models import Notification
+from jobsPy.notifications.models import NotificationJobSeeker
 
 
 class NotificationListView(LoginRequiredMixin, ListView):
-    model = Notification
+    model = NotificationJobSeeker
     template_name = 'notifications/notifications.html'
     context_object_name = 'notifications'
 
     def get_queryset(self):
+        print(self.request.user)
+        print(self)
         return super().get_queryset().filter(user=self.request.user)

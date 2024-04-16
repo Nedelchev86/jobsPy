9ae23f0f5eee97a8a366f3d7dd6257ba50b50ee7
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-16 21:53:12 +0300

    Added notification for the job seeker when the company changes the status of their application.

--- jobsPy/notifications/models.py
+++ jobsPy/notifications/models.py
@@ -10,6 +10,19 @@ UserModel = get_user_model()
 class Notification(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    job_seeker = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='job_seeker')
     message = models.CharField(max_length=600)
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+
+class NotificationJobSeeker(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20)
+    comment = models.CharField(max_length=600)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]

28c77d6b02b939ec249369dfbcb01c559c23ef0d
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-19 18:51:24 +0300

    Trying to resolve celery problem after deploy

--- azure.sh
+++ azure.sh
@@ -1,1 +1,1 @@
-gunicorn --bind=0.0.0.0 --timeout 600 jobsPy.wsgi & celery -A jobsPy worker --loglevel=info
+gunicorn --bind=0.0.0.0 --timeout 600 jobsPy.wsgi & celery -A jobsPy worker --loglevel=info -P eventlet -E

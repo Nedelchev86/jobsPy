1332a18a9d79a0a50580113ae7b6e0dea8f66219
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-05-10 23:05:17 +0300

    Add createContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,24 +11,30 @@ import JobsList from "./components/JobsList";
 import CompanyDetails from "./components/CompanyDetails";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
+import {AuthProvider} from "./contexts/Contexts"; // Import AuthProvider
+import Dashboard from "./components/Dashboard";
 
 function App() {
     return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/companies" element={<CompanyList />} />
-                <Route path="/company/:id" element={<CompanyDetails />} />
-                <Route path="/jobseekers" element={<JobseekersList />} />
-                <Route path="/jobseeker/:id" element={<JobSeekerDetails />} />
-                <Route path="/jobs" element={<JobsList />} />
-                <Route path="/signup" element={<RegisterForm />} />
-                <Route path="/login" element={<LoginForm />} />
-            </Routes>
-            <Footer />
-        </Router>
+        <>
+            {" "}
+            <AuthProvider>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/companies" element={<CompanyList />} />
+                    <Route path="/company/:id" element={<CompanyDetails />} />
+                    <Route path="/jobseekers" element={<JobseekersList />} />
+                    <Route path="/jobseeker/:id" element={<JobSeekerDetails />} />
+                    <Route path="/jobs" element={<JobsList />} />
+                    <Route path="/signup" element={<RegisterForm />} />
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="*" element={<Dashboard />} />
+                </Routes>
+                <Footer />
+            </AuthProvider>
+        </>
     );
 }
 

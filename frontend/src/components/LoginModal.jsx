c18203de89edf9c06f04b25c0139a09ad9202dc8
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-05-10 23:06:44 +0300

    Change login to work with context

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import {Modal, Button, Form} from "react-bootstrap";
+import {useAuth} from "../contexts/Contexts";
 
 const LoginModal = ({show, handleClose}) => {
+    const {login} = useAuth();
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await login(formData);
+            handleClose(); // Close the modal after successful login
+        } catch (error) {
+            console.error("Login failed:", error.message);
+            setError("Login failed. Please check your credentials.");
+        }
+    };
+
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title>Login From Here</Modal.Title>
+                <Modal.Title>Login</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
+                <Form onSubmit={handleSubmit}>
+                    {error && <div className="alert alert-danger">{error}</div>}
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleChange} required />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Remember password" />
+                        <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange} required />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Log in

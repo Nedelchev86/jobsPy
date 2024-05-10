b96c1c643ffdb08e567866793302dc0415b7b70c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-05-10 23:06:15 +0300

    Change Header.jsx to work with context

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,50 +1,182 @@
-import React, {useState, useEffect} from "react";
-import {Link, useNavigate} from "react-router-dom";
+// import React, {useState, useEffect} from "react";
+// import {Link, useNavigate} from "react-router-dom";
 
-import LoginModal from "./LoginForm";
-import {jwtDecode} from "jwt-decode";
+// import LoginModal from "./LoginForm";
+// import {jwtDecode} from "jwt-decode";
 
-export default function Header() {
-    const isAuthenticated = localStorage.getItem("token");
-    const [user, setUser] = useState(null);
-    const history = useNavigate();
+// export default function Header() {
+//     const isAuthenticated = localStorage.getItem("token");
+//     const [user, setUser] = useState(null);
+//     const history = useNavigate();
+//     const [showModal, setShowModal] = useState(false);
+//     const toggleModal = () => {
+//         setShowModal(!showModal);
+//     };
+
+//     const handleLogout = () => {
+//         localStorage.removeItem("token");
+//         history("/");
+//     };
+
+//     useEffect(() => {
+//         if (!isAuthenticated) {
+//             return;
+//         }
+
+//         fetch("http://127.0.0.1:8000/api/user/", {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization: `Bearer ${isAuthenticated}`,
+//             },
+//         })
+//             .then((response) => {
+//                 if (!response.ok) {
+//                     throw new Error("Failed to fetch user data");
+//                 }
+//                 return response.json();
+//             })
+//             .then((data) => {
+//                 setUser(data);
+//             })
+//             .catch((error) => {
+//                 console.error("Error fetching user data:", error);
+//             });
+//     }, [isAuthenticated]);
+
+//     return (
+//         <header className="header style4">
+//             <div className="navbar-area">
+//                 <div className="container">
+//                     <div className="row align-items-center">
+//                         <div className="col-lg-12">
+//                             <nav className="navbar navbar-expand-lg">
+//                                 <a className="navbar-brand logo" href="#">
+//                                     <img className="logo1" src="/images/logo/logo.svg" alt="Logo" />
+//                                 </a>
+//                                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                                     <span className="toggler-icon"></span>
+//                                     <span className="toggler-icon"></span>
+//                                     <span className="toggler-icon"></span>
+//                                 </button>
+
+//                                 <div className="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+//                                     <ul id="nav" className="navbar-nav ml-auto">
+//                                         <li className="nav-item">
+//                                             <Link to="/" className="nav-link">
+//                                                 Home
+//                                             </Link>
+//                                         </li>
+
+//                                         <li className="nav-item ">
+//                                             <Link to="/jobs" className="nav-link">
+//                                                 Jobs
+//                                             </Link>
+//                                         </li>
+//                                         <li className="nav-item">
+//                                             <Link to="/companies" className="nav-link">
+//                                                 Companies
+//                                             </Link>
+//                                         </li>
+//                                         <li className="nav-item">
+//                                             <Link to="/jobseekers" className="nav-link">
+//                                                 Jobseekers
+//                                             </Link>
+//                                         </li>
+//                                         <li className="nav-item">
+//                                             <Link to="/blog" className="nav-link">
+//                                                 Blog
+//                                             </Link>
+//                                         </li>
+//                                         <li className="nav-item">
+//                                             <a href="#" className="">
+//                                                 Contact
+//                                             </a>
+//                                         </li>
+//                                     </ul>
+//                                 </div>
+
+//                                 {!isAuthenticated && (
+//                                     <div className="button">
+//                                         {/* <Link to="/login" className="login">
+//                                             Login
+//                                         </Link> */}
+//                                         <Link onClick={toggleModal} className="login">
+//                                             <i className="lni lni-lock-alt"></i>
+//                                             Login
+//                                         </Link>
+
+//                                         <Link to="/signup" className="btn">
+//                                             Sign Up
+//                                         </Link>
+//                                     </div>
+//                                 )}
+
+//                                 {isAuthenticated && user && (
+//                                     <>
+//                                         <div
+//                                             className="text-center align-center"
+//                                             style={{
+//                                                 alignItems: "center !important",
+//                                                 justifyContent: "center !important",
+//                                                 backgroundColor: "black",
+//                                                 width: 50,
+//                                                 height: 50,
+//                                                 borderRadius: 50,
+//                                                 marginRight: 10,
+//                                                 marginLeft: 20,
+//                                             }}
+//                                         >
+//                                             <div className="user-profile-container">
+//                                                 {/* {user && ( */}
+//                                                 <a href="#" className="user-profile-small" style={{paddingTop: "10%", color: "white", fontSize: 25}}>
+//                                                     {user.email.split("")[0]}
+//                                                     <span className="notification-badge">0</span>
+//                                                 </a>
+//                                             </div>
+//                                         </div>
+//                                         <div className="button">
+//                                             <button onClick={handleLogout} className="btn">
+//                                                 Logout
+//                                             </button>
+//                                         </div>
+//                                     </>
+//                                 )}
+//                             </nav>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             <LoginModal show={showModal} handleClose={toggleModal} />
+//         </header>
+//     );
+// }
+
+import React, {useState} from "react";
+import {Button} from "react-bootstrap";
+import {useAuth} from "../contexts/Contexts";
+import LoginForm from "./LoginForm";
+import {Link} from "react-router-dom";
+
+const Header = () => {
+    const {user, isAuthenticated, login, logout} = useAuth();
+    // Check if the user is authenticated
+
     const [showModal, setShowModal] = useState(false);
-    const toggleModal = () => {
-        setShowModal(!showModal);
+
+    const handleOpenModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
+    const handleSubmitLogin = async (formData) => {
+        // Call the login function with form data and open modal callback
+        await login(formData, handleOpenModal);
     };
-
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        history("/");
-    };
-
-    useEffect(() => {
-        if (!isAuthenticated) {
-            return;
-        }
-
-        fetch("http://127.0.0.1:8000/api/user/", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${isAuthenticated}`,
-            },
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Failed to fetch user data");
-                }
-                return response.json();
-            })
-            .then((data) => {
-                setUser(data);
-            })
-            .catch((error) => {
-                console.error("Error fetching user data:", error);
-            });
-    }, []);
 
     return (
+        // <>
+        //     <div>{isAuthenticated ? <button onClick={logout}>Logout</button> : <button onClick={handleOpenModal}>Login</button>}</div>
+        //     <LoginForm show={showModal} handleClose={handleCloseModal} />
+        // </>
         <header className="header style4">
             <div className="navbar-area">
                 <div className="container">
@@ -93,19 +225,20 @@ export default function Header() {
                                                 Contact
                                             </a>
                                         </li>
+                                        {isAuthenticated && (
+                                            <li className="nav-item">
+                                                <Link to="/dashboard">Dashboard</Link>
+                                            </li>
+                                        )}
                                     </ul>
                                 </div>
 
                                 {!isAuthenticated && (
                                     <div className="button">
-                                        {/* <Link to="/login" className="login">
-                                            Login
-                                        </Link> */}
-                                        <Link onClick={toggleModal} className="login">
+                                        <Link onClick={handleOpenModal} className="login">
                                             <i className="lni lni-lock-alt"></i>
                                             Login
                                         </Link>
-
                                         <Link to="/signup" className="btn">
                                             Sign Up
                                         </Link>
@@ -128,9 +261,6 @@ export default function Header() {
                                             }}
                                         >
                                             <div className="user-profile-container">
-                                                {/* <a href="#">
-                                                <img className="user-profile-small" src="" alt="Logo" /> <span className="notification-badge">0</span>
-                                            </a> */}
                                                 {user && (
                                                     <a href="#" className="user-profile-small" style={{paddingTop: "10%", color: "white", fontSize: 25}}>
                                                         {user.email.split("")[0]}
@@ -140,7 +270,7 @@ export default function Header() {
                                             </div>
                                         </div>
                                         <div className="button">
-                                            <button onClick={handleLogout} className="btn">
+                                            <button onClick={logout} className="btn">
                                                 Logout
                                             </button>
                                         </div>
@@ -151,7 +281,9 @@ export default function Header() {
                     </div>
                 </div>
             </div>
-            <LoginModal show={showModal} handleClose={toggleModal} />
+            <LoginForm show={showModal} handleClose={handleCloseModal} />
         </header>
     );
-}
+};
+
+export default Header;

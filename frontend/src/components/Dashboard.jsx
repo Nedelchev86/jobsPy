5da4402d6116667ac29b37621589693c3512e434
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-05-12 18:53:22 +0300

    Add JobSeekers Dashboard component

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,8 +1,34 @@
+import {useEffect} from "react";
+import {useAuth} from "../contexts/Contexts";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import JobSeekerMenu from "./JobseekerMenu";
+import Breadcrumbs from "./Breadcrumbs";
 export default function Dashboard() {
+    const {user, isAuthenticated} = useAuth();
+    const navigate = useNavigate();
+
+    if (!isAuthenticated) {
+        return <div>Not authenticated...</div>;
+    }
+
+    if (!user) {
+        // Render loading state or redirect to login page
+        console.log("loading");
+        return <div>Loading...</div>;
+    }
+
     return (
-        <div className="bookmarked section">
-            <div className="container">
-                {/* {% if messages %}
+        <>
+            {" "}
+            <Breadcrumbs
+                pageTitle="Profile Dashboard"
+                pageInfo="Editing and updating a profile profile allows you to keep information current and accurate,
+                while also allowing companies to find you."
+            />
+            <div className="bookmarked section">
+                <div className="container"> 
+                    {/* {% if messages %}
             <div className="alert alert-danger" role="alert">
                 
                 <ul className="messages">
@@ -12,53 +38,58 @@ export default function Dashboard() {
                 </ul>
             </div>
             {% endif %} */}
-                <div className="alerts-inner">
-                    <div className="row">
-                        {/* {% include 'job_seekers/job_seeker_menu.html' %} */}
+                    <div className="alerts-inner">
+                        <div className="row">
+                            <JobSeekerMenu />
 
-                        <div className="col-lg-8 col-12">
-                            <div className="cat-head job-category">
-                                <div className="row">
-                                    <div className="col-md-4 col-12">
-                                        <a href="{% url 'jobs-apply' %}" className="single-cat wow fadeInUp" data-wow-delay=".8s">
-                                            <div className="icon">{/* <i>{{all_jobs_accepted}}</i> */}</div>
-                                            <h3>
-                                                Approved <br /> applications
-                                            </h3>
-                                        </a>
-                                    </div>
-                                    <div className="col-md-4 col-12">
-                                        <a href="{% url 'jobs-apply' %}" className="single-cat wow fadeInUp" data-wow-delay=".2s">
-                                            <div className="icon">{/* <i>{{all_jobs_pending}}</i> */}</div>
-                                            <h3>
-                                                Pending
-                                                <br /> applications
-                                            </h3>
-                                        </a>
+                            <div className="col-lg-8 col-12">
+                                <div className="cat-head job-category">
+                                    <div className="row">
+                                        <div className="col-md-4 col-12">
+                                            <a href="{% url 'jobs-apply' %}" className="single-cat wow fadeInUp" data-wow-delay=".8s">
+                                                <div className="icon">{/* <i>{{all_jobs_accepted}}</i> */}</div>
+                                                <h3>
+                                                    Approved <br /> applications
+                                                </h3>
+                                            </a>
+                                        </div>
+                                        <div className="col-md-4 col-12">
+                                            <a href="{% url 'jobs-apply' %}" className="single-cat wow fadeInUp" data-wow-delay=".2s">
+                                                <div className="icon">{/* <i>{{all_jobs_pending}}</i> */}</div>
+                                                <h3>
+                                                    Pending
+                                                    <br /> applications
+                                                </h3>
+                                            </a>
+                                        </div>
+
+                                        <div className="col-md-4 col-12">
+                                            <a href="{% url 'jobs-apply' %}" className="single-cat wow fadeInUp" data-wow-delay=".2s">
+                                                <div className="icon">{/* <i>{{all_jobs_rejected}}</i> */}</div>
+                                                <h3>
+                                                    Rejected
+                                                    <br /> applications
+                                                </h3>
+                                            </a>
+                                        </div>
                                     </div>
 
-                                    <div className="col-md-4 col-12">
-                                        <a href="{% url 'jobs-apply' %}" className="single-cat wow fadeInUp" data-wow-delay=".2s">
-                                            <div className="icon">{/* <i>{{all_jobs_rejected}}</i> */}</div>
-                                            <h3>
-                                                Rejected
-                                                <br /> applications
-                                            </h3>
-                                        </a>
-                                    </div>
-                                </div>
-
-                                <div className="resume ">
-                                    <div className="container">
-                                        <div className="resume-inner">
-                                            <div className="row">
-                                                <div className="col-lg-12 col-12">
-                                                    <div className="inner-content">
-                                                        <div className="personal-top-content">
-                                                            <div className="row">
-                                                                <div className="col-lg-5 col-md-5 col-12">
-                                                                    <div className="name-head">
-                                                                        {/* {% if user.jobseeker.profile_picture %}
+                                    <div className="resume ">
+                                        <div className="container">
+                                            <div className="resume-inner">
+                                                <div className="row">
+                                                    <div className="col-lg-12 col-12">
+                                                        <div className="inner-content">
+                                                            <div className="personal-top-content">
+                                                                <div className="row">
+                                                                    <div className="col-lg-5 col-md-5 col-12">
+                                                                        <div className="name-head">
+                                                                            {user.user.profile_picture && (
+                                                                                <a className="mb-2" href="#">
+                                                                                    <img className="circle-54" src={`https://res.cloudinary.com/drjgddl0y/${user.user.profile_picture}`} alt="" />
+                                                                                </a>
+                                                                            )}
+                                                                            {/* {% if user.jobseeker.profile_picture %}
                                             <a className="mb-2" href="#"><img className="circle-54"
                                                     src="{{ user.jobseeker.profile_picture.url}}" alt=""></a>
                                             {% endif %} 
@@ -77,63 +108,62 @@ export default function Dashboard() {
                                                 <li><a target="_blank" href="{{ user.jobseeker.github }}"><i className="lni lni-github"></i></a></li>
                                                 {% endif %}
                                             </ul> */}
+                                                                        </div>
                                                                     </div>
-                                                                </div>
-                                                                <div className="col-lg-7 col-md-7 col-12">
-                                                                    <div className="content-right">
-                                                                        <h5 className="title-main">Profile Information</h5>
+                                                                    <div className="col-lg-7 col-md-7 col-12">
+                                                                        <div className="content-right">
+                                                                            <h5 className="title-main">Profile Information</h5>
 
-                                                                        <div className="single-list">
-                                                                            <h5 className="title">Location</h5>
-                                                                            {/* <p>{{ request.user.jobseeker.city }}</p> */}
-                                                                        </div>
+                                                                            <div className="single-list">
+                                                                                <h5 className="title">Location</h5>
+                                                                                <p>{user.user.city}</p>
+                                                                            </div>
 
-                                                                        <div className="single-list">
-                                                                            <h5 className="title">E-mail</h5>
-                                                                            {/* <p> {{request.user}} </p> */}
-                                                                        </div>
+                                                                            <div className="single-list">
+                                                                                <h5 className="title">E-mail</h5>
+                                                                                <p> {user.email} </p>
+                                                                            </div>
 
-                                                                        <div className="single-list">
-                                                                            <h5 className="title">Phone</h5>
-                                                                            {/* <p>{{request.user.jobseeker.phone_number}}</p> */}
-                                                                        </div>
+                                                                            <div className="single-list">
+                                                                                <h5 className="title">Phone</h5>
+                                                                                <p>{user.user.phone_number}</p>
+                                                                            </div>
 
-                                                                        <div className="single-list">
-                                                                            <h5 className="title">Website</h5>
-                                                                            <p>
-                                                                                <a target="_blank" href="{{ user.jobseeker.website  }}">
-                                                                                    {" "}
-                                                                                    Link{" "}
-                                                                                </a>
-                                                                            </p>
+                                                                            <div className="single-list">
+                                                                                <h5 className="title">Website</h5>
+                                                                                <p>
+                                                                                    <a target="_blank" href={user.user.website}>
+                                                                                        Link
+                                                                                    </a>
+                                                                                </p>
+                                                                            </div>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
 
-                                                        <div className="single-section">
-                                                            <h4>About</h4>
-                                                            <p className="font-size-4 mb-8">{/* {{ request.user.jobseeker.about|safe }} */}</p>
-                                                        </div>
+                                                            <div className="single-section">
+                                                                <h4>About</h4>
+                                                                <p className="font-size-4 mb-8">{user.user.about}</p>
+                                                            </div>
 
-                                                        <div className="single-section skill">
-                                                            <h4>Skills</h4>
-                                                            <ul className="list-unstyled d-flex align-items-center flex-wrap">
-                                                                {/* {%  for skill in request.user.jobseeker.skills.all %}
+                                                            <div className="single-section skill">
+                                                                <h4>Skills</h4>
+                                                                <ul className="list-unstyled d-flex align-items-center flex-wrap">
+                                                                    {/* {%  for skill in request.user.jobseeker.skills.all %}
                                       <li>
                                         <a href="#">{{ skill}}</a>
                                     </li>
                                     {% endfor %} */}
-                                                            </ul>
-                                                        </div>
+                                                                </ul>
+                                                            </div>
 
-                                                        <div className="single-section exprerience">
-                                                            {/* <a style="float: right" className="education-link" href="{% url 'add-work-experience' user.jobseeker.pk %}">
+                                                            <div className="single-section exprerience">
+                                                                {/* <a style="float: right" className="education-link" href="{% url 'add-work-experience' user.jobseeker.pk %}">
                                                                 Add Work Experience{" "}
                                                             </a> */}
-                                                            <h4>Work Experience</h4>
-                                                            {/* 
+                                                                <h4>Work Experience</h4>
+                                                                {/* 
                                 {% for experience in user.jobseeker.experience.all %}
        
                                     
@@ -164,15 +194,15 @@ export default function Dashboard() {
                                     </div>
                                 </div>
                                 {% endfor %} */}
-                                                        </div>
+                                                            </div>
 
-                                                        <div className="single-section education">
-                                                            {/* <a style="float: right" className="education-link" href="{% url 'add-education' user.jobseeker.pk %}">
+                                                            <div className="single-section education">
+                                                                {/* <a style="float: right" className="education-link" href="{% url 'add-education' user.jobseeker.pk %}">
                                                                 Add Education
                                                             </a> */}
-                                                            <h4>Education</h4>
+                                                                <h4>Education</h4>
 
-                                                            {/* {%  for education in request.user.jobseeker.educations.all %}
+                                                                {/* {%  for education in request.user.jobseeker.educations.all %}
                                 <div className="single-edu mb-30">
                                     <div className="d-flex align-items-center pr-11 mb-9 flex-wrap flex-sm-nowrap">
                                         <div className="image" >
@@ -198,6 +228,7 @@ export default function Dashboard() {
                                     </div>
                                 </div>
                                 {% endfor %} */}
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -210,6 +241,6 @@ export default function Dashboard() {
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }

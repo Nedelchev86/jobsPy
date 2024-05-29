ce1bacf4742fb79a19cdce65377f512b71768ddf
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-05-29 20:55:34 +0300

    Add more route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,28 +13,52 @@ import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
 import {AuthProvider} from "./contexts/Contexts"; // Import AuthProvider
 import Dashboard from "./components/Dashboard";
+import JobDetails from "./components/JobDetails";
+import JobsFavoriteList from "./components/JobsFavoriteList";
+import Breadcrumbs from "./components/Breadcrumbs";
+import JobSeekerDashboard from "./components/JobSeekerDashboard";
+import JobSeekerLayout from "./components/JobSeekerLayout";
+import EditProfile from "./components/EditProfile";
+import CreateJob from "./components/CreateJob";
+import ApplicantsList from "./components/ApplicantsList";
+import CreatedJobs from "./components/CreatedJobs";
+import ChangePassword from "./components/ChangePassword";
+import {JobProvider} from "./components/JobContext";
 
 function App() {
     return (
-        <>
-            {" "}
-            <AuthProvider>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Index />} />
-                    <Route path="/blog" element={<Blog />} />
-                    <Route path="/companies" element={<CompanyList />} />
-                    <Route path="/company/:id" element={<CompanyDetails />} />
-                    <Route path="/jobseekers" element={<JobseekersList />} />
-                    <Route path="/jobseeker/:id" element={<JobSeekerDetails />} />
-                    <Route path="/jobs" element={<JobsList />} />
-                    <Route path="/signup" element={<RegisterForm />} />
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="*" element={<Dashboard />} />
-                </Routes>
-                <Footer />
-            </AuthProvider>
-        </>
+        <AuthProvider>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="blog" element={<Blog />} />
+                {/* <Route path="companies" element={<CompanyList />} /> */}
+                <Route path="companies">
+                    <Route index element={<CompanyList />} />
+                    <Route path=":id" element={<CompanyDetails />} />
+                </Route>
+                <Route path="companies/:id" element={<CompanyDetails />} />
+                <Route path="jobseekers" element={<JobseekersList />} />
+                <Route path="jobseekers/:id" element={<JobSeekerDetails />} />
+                <Route path="jobs" element={<JobsList />} />
+                <Route path="signup" element={<RegisterForm />} />
+                <Route path="login" element={<LoginForm />} />
+                {/* <Route path="dashboard" element={<Dashboard />} /> */}
+                {/* <Route path="/bookmarked" element={<JobsFavoriteList />} /> */}
+                {/* <Route path="/profile/edit" element={<EditProfile />} /> */}
+                <Route path="jobs/:id" element={<JobDetails />} />
+                <Route path="dashboard" element={<JobSeekerLayout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="edit" element={<EditProfile />} />
+                    <Route path="bookmarked" element={<JobsFavoriteList />} />
+                    <Route path="create-job" element={<CreateJob />} />
+                    <Route path="applicants" element={<ApplicantsList />} />
+                    <Route path="created-jobs" element={<CreatedJobs />} />
+                    <Route path="change-password" element={<ChangePassword />} />
+                </Route>
+            </Routes>
+            <Footer />
+        </AuthProvider>
     );
 }
 

1be5a0fcef180af1147d24f73ed5e33a82c90526
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-06-01 18:25:05 +0300

    Add more components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,165 +1,13 @@
-// import React, {useState, useEffect} from "react";
-// import {Link, useNavigate} from "react-router-dom";
-
-// import LoginModal from "./LoginForm";
-// import {jwtDecode} from "jwt-decode";
-
-// export default function Header() {
-//     const isAuthenticated = localStorage.getItem("token");
-//     const [user, setUser] = useState(null);
-//     const history = useNavigate();
-//     const [showModal, setShowModal] = useState(false);
-//     const toggleModal = () => {
-//         setShowModal(!showModal);
-//     };
-
-//     const handleLogout = () => {
-//         localStorage.removeItem("token");
-//         history("/");
-//     };
-
-//     useEffect(() => {
-//         if (!isAuthenticated) {
-//             return;
-//         }
-
-//         fetch("http://127.0.0.1:8000/api/user/", {
-//             method: "GET",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 Authorization: `Bearer ${isAuthenticated}`,
-//             },
-//         })
-//             .then((response) => {
-//                 if (!response.ok) {
-//                     throw new Error("Failed to fetch user data");
-//                 }
-//                 return response.json();
-//             })
-//             .then((data) => {
-//                 setUser(data);
-//             })
-//             .catch((error) => {
-//                 console.error("Error fetching user data:", error);
-//             });
-//     }, [isAuthenticated]);
-
-//     return (
-//         <header className="header style4">
-//             <div className="navbar-area">
-//                 <div className="container">
-//                     <div className="row align-items-center">
-//                         <div className="col-lg-12">
-//                             <nav className="navbar navbar-expand-lg">
-//                                 <a className="navbar-brand logo" href="#">
-//                                     <img className="logo1" src="/images/logo/logo.svg" alt="Logo" />
-//                                 </a>
-//                                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                                     <span className="toggler-icon"></span>
-//                                     <span className="toggler-icon"></span>
-//                                     <span className="toggler-icon"></span>
-//                                 </button>
-
-//                                 <div className="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-//                                     <ul id="nav" className="navbar-nav ml-auto">
-//                                         <li className="nav-item">
-//                                             <Link to="/" className="nav-link">
-//                                                 Home
-//                                             </Link>
-//                                         </li>
-
-//                                         <li className="nav-item ">
-//                                             <Link to="/jobs" className="nav-link">
-//                                                 Jobs
-//                                             </Link>
-//                                         </li>
-//                                         <li className="nav-item">
-//                                             <Link to="/companies" className="nav-link">
-//                                                 Companies
-//                                             </Link>
-//                                         </li>
-//                                         <li className="nav-item">
-//                                             <Link to="/jobseekers" className="nav-link">
-//                                                 Jobseekers
-//                                             </Link>
-//                                         </li>
-//                                         <li className="nav-item">
-//                                             <Link to="/blog" className="nav-link">
-//                                                 Blog
-//                                             </Link>
-//                                         </li>
-//                                         <li className="nav-item">
-//                                             <a href="#" className="">
-//                                                 Contact
-//                                             </a>
-//                                         </li>
-//                                     </ul>
-//                                 </div>
-
-//                                 {!isAuthenticated && (
-//                                     <div className="button">
-//                                         {/* <Link to="/login" className="login">
-//                                             Login
-//                                         </Link> */}
-//                                         <Link onClick={toggleModal} className="login">
-//                                             <i className="lni lni-lock-alt"></i>
-//                                             Login
-//                                         </Link>
-
-//                                         <Link to="/signup" className="btn">
-//                                             Sign Up
-//                                         </Link>
-//                                     </div>
-//                                 )}
-
-//                                 {isAuthenticated && user && (
-//                                     <>
-//                                         <div
-//                                             className="text-center align-center"
-//                                             style={{
-//                                                 alignItems: "center !important",
-//                                                 justifyContent: "center !important",
-//                                                 backgroundColor: "black",
-//                                                 width: 50,
-//                                                 height: 50,
-//                                                 borderRadius: 50,
-//                                                 marginRight: 10,
-//                                                 marginLeft: 20,
-//                                             }}
-//                                         >
-//                                             <div className="user-profile-container">
-//                                                 {/* {user && ( */}
-//                                                 <a href="#" className="user-profile-small" style={{paddingTop: "10%", color: "white", fontSize: 25}}>
-//                                                     {user.email.split("")[0]}
-//                                                     <span className="notification-badge">0</span>
-//                                                 </a>
-//                                             </div>
-//                                         </div>
-//                                         <div className="button">
-//                                             <button onClick={handleLogout} className="btn">
-//                                                 Logout
-//                                             </button>
-//                                         </div>
-//                                     </>
-//                                 )}
-//                             </nav>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//             <LoginModal show={showModal} handleClose={toggleModal} />
-//         </header>
-//     );
-// }
-
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import {useAuth} from "../contexts/Contexts";
 import LoginForm from "./LoginForm";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
+import {useJobs} from "./JobContext";
 
 const Header = () => {
     const {user, isAuthenticated, login, logout} = useAuth();
+    const {notifications, fetchNotifications} = useJobs();
     // Check if the user is authenticated
 
     const [showModal, setShowModal] = useState(false);
@@ -171,12 +19,14 @@ const Header = () => {
         // Call the login function with form data and open modal callback
         await login(formData, handleOpenModal);
     };
+    useEffect(() => {
+        // Fetch notifications when the component mounts
+        if (isAuthenticated) {
+            fetchNotifications();
+        }
+    }, [isAuthenticated, notifications.length]);
 
     return (
-        // <>
-        //     <div>{isAuthenticated ? <button onClick={logout}>Logout</button> : <button onClick={handleOpenModal}>Login</button>}</div>
-        //     <LoginForm show={showModal} handleClose={handleCloseModal} />
-        // </>
         <header className="header style4">
             <div className="navbar-area">
                 <div className="container">
@@ -195,30 +45,30 @@ const Header = () => {
                                 <div className="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                                     <ul id="nav" className="navbar-nav ml-auto">
                                         <li className="nav-item">
-                                            <Link to="/" className="nav-link">
+                                            <NavLink to="/" activeclassname="active" className="nav-link">
                                                 Home
-                                            </Link>
+                                            </NavLink>
                                         </li>
 
                                         <li className="nav-item ">
-                                            <Link to="/jobs" className="nav-link">
+                                            <NavLink to="/jobs" activeclassname="active" className="nav-Navlink">
                                                 Jobs
-                                            </Link>
+                                            </NavLink>
                                         </li>
                                         <li className="nav-item">
-                                            <Link to="/companies" className="nav-link">
+                                            <NavLink to="/companies" activeclassname="active" className="nav-Navlink">
                                                 Companies
-                                            </Link>
+                                            </NavLink>
                                         </li>
                                         <li className="nav-item">
-                                            <Link to="/jobseekers" className="nav-link">
+                                            <NavLink to="/jobseekers" activeclassname="active" className="nav-Navlink">
                                                 Jobseekers
-                                            </Link>
+                                            </NavLink>
                                         </li>
                                         <li className="nav-item">
-                                            <Link to="/blog" className="nav-link">
+                                            <NavLink to="/blog" activeclassname="active" className="nav-Navlink">
                                                 Blog
-                                            </Link>
+                                            </NavLink>
                                         </li>
                                         <li className="nav-item">
                                             <a href="#" className="">
@@ -227,7 +77,9 @@ const Header = () => {
                                         </li>
                                         {isAuthenticated && (
                                             <li className="nav-item">
-                                                <Link to="/dashboard">Dashboard</Link>
+                                                <NavLink to="/dashboard" activeclassname="active">
+                                                    Dashboard
+                                                </NavLink>
                                             </li>
                                         )}
                                     </ul>
@@ -235,17 +87,47 @@ const Header = () => {
 
                                 {!isAuthenticated && (
                                     <div className="button">
-                                        <Link onClick={handleOpenModal} className="login">
+                                        <NavLink onClick={handleOpenModal} className="login">
                                             <i className="lni lni-lock-alt"></i>
                                             Login
-                                        </Link>
-                                        <Link to="/signup" className="btn">
+                                        </NavLink>
+                                        <NavLink to="/signup" className="btn">
                                             Sign Up
-                                        </Link>
+                                        </NavLink>
                                     </div>
                                 )}
 
-                                {isAuthenticated && (
+                                {isAuthenticated && user && user.user_type === "company" && user.user.image && (
+                                    <div className="button">
+                                        <div className="user-profile-container">
+                                            <NavLink to="dashboard">
+                                                <img className="user-profile-small" src={`https://res.cloudinary.com/drjgddl0y/${user.user.image}`} alt="Logo" /> <span className="notification-badge"> {notifications.length} </span>
+                                            </NavLink>
+                                        </div>
+
+                                        <button onClick={logout} className="btn">
+                                            Logout
+                                        </button>
+                                    </div>
+                                )}
+
+                                {isAuthenticated && user && user.user_type === "jobseeker" && user.user.profile_picture && (
+                                    <div className="button">
+                                        <div className="user-profile-container">
+                                            <NavLink to="dashboard">
+                                                <img className="user-profile-small" src={`https://res.cloudinary.com/drjgddl0y/${user.user.profile_picture}`} alt="Logo" /> <span className="notification-badge"> 0 </span>{" "}
+                                            </NavLink>
+                                        </div>
+                                        <button onClick={logout} className="btn">
+                                            Logout
+                                        </button>
+                                        {/* <a href="{% url 'sing-out' %}" className="login">
+                                            <i className="lni lni-lock-alt"></i> Logout
+                                        </a> */}
+                                    </div>
+                                )}
+
+                                {isAuthenticated && user && !user.user.profile_picture && !user.user.image && (
                                     <>
                                         <div
                                             className="text-center align-center"

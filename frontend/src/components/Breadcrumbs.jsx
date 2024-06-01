8a1ae410ae28240daeb187c6faa269cfb88405a5
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-06-01 18:23:55 +0300

    Add breadcrumbs.jsx

--- frontend/src/components/Breadcrumbs.jsx
+++ frontend/src/components/Breadcrumbs.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 const Breadcrumbs = ({pageTitle, pageInfo}) => {
     return (
-        <div className="breadcrumbs overlay">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="breadcrumbs-content">
-                            <h1 className="page-title">{pageTitle}</h1>
-                            <p>{pageInfo}</p>
+        <>
+            <div className="breadcrumbs overlay">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="breadcrumbs-content">
+                                <h1 className="page-title">{pageTitle}</h1>
+                                <p>{pageInfo}</p>
+                            </div>
+                            <ul className="breadcrumb-nav">
+                                <li>
+                                    <Link href="/">Home</Link>
+                                </li>
+                                <li>Companies</li>
+                            </ul>
                         </div>
-                        <ul className="breadcrumb-nav">
-                            <li>
-                                <Link href="/">Home</Link>
-                            </li>
-                            <li>Companies</li>
-                        </ul>
                     </div>
                 </div>
             </div>
-        </div>
+            {/* <Outlet /> */}
+        </>
     );
 };
 
